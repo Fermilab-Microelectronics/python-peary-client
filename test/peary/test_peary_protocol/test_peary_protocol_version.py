bfb588d15475228a1ab9de5fb0a4fc64d60ad899
Jeff Fredenburg <jfreden@fnal.gov>
2025-03-18 19:42:36 -0500

    simplify names used in the protocol version test

--- test/peary/test_peary_protocol/test_peary_protocol_version.py
+++ test/peary/test_peary_protocol/test_peary_protocol_version.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
 
 def test_peary_protocol_version_request_message(mock_socket: type[MockSocket]) -> None:
 
-    class MockPearyProtocol(PearyProtocol):
+    class MockProtocol(PearyProtocol):
 
         def request(
             self, msg: str, *args: str, buffer_size: int = 4096  # noqa: ARG002
@@ -20,24 +20,24 @@ def test_peary_protocol_version_request_message(mock_socket: type[MockSocket]) -
             assert msg == "protocol_version"
             return b"1"
 
-    MockPearyProtocol(mock_socket())
+    MockProtocol(mock_socket())
 
 
-def test_peary_protocol_init_version_supported(mock_socket: type[MockSocket]) -> None:
+def test_peary_protocol_version_supported(mock_socket: type[MockSocket]) -> None:
 
-    class MockPearyProtocol(PearyProtocol):
+    class MockProtocol(PearyProtocol):
 
         def request(
             self, msg: str, *args: str, buffer_size: int = 4096  # noqa: ARG002
         ) -> bytes:
             return b"1"
 
-    MockPearyProtocol(mock_socket())
+    MockProtocol(mock_socket())
 
 
-def test_peary_protocol_init_version_unsupported(mock_socket: type[MockSocket]) -> None:
+def test_peary_protocol_version_unsupported(mock_socket: type[MockSocket]) -> None:
 
-    class MockPearyProtocol(PearyProtocol):
+    class MockProtocol(PearyProtocol):
 
         def request(
             self, msg: str, *args: str, buffer_size: int = 4096  # noqa: ARG002
@@ -45,7 +45,6 @@ def test_peary_protocol_init_version_unsupported(mock_socket: type[MockSocket]) 
             return b"0"
 
     with pytest.raises(
-        PearyProtocol.IncompatibleProtocolError,
-        match="Unsupported protocol version: b'0'",
+        PearyProtocol.VersionError, match="Unsupported protocol version: b'0'"
     ):
-        MockPearyProtocol(mock_socket())
+        MockProtocol(mock_socket())
